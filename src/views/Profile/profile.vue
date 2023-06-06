48092e59bbddbdc5d4a8bb7a6e4c91611a950417
aluno19262 <aluno19262@ipt.pt>
2023-06-06 09:32:53 +0100

    fix password fields on profile form

--- src/views/Profile/profile.vue
+++ src/views/Profile/profile.vue
@@ -14,9 +14,9 @@
         </v-row>
         <v-row>
           <v-text-field
-            v-model="password"
+            v-model.trim="password"
             label="Palavra passe"
-            :rules="fieldRules"
+            :rules="rules"
             :type="showPassword ? 'text' : 'password'"
             :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append-inner="showPassword = !showPassword"
@@ -25,9 +25,9 @@
         </v-row>
         <v-row>
           <v-text-field
-            v-model="confirmPassword"
+            v-model.trim="confirmPassword"
             label="Confirmar Palavra passe"
-            :rules="confirmPasswordRules"
+            :rules="rules"
             :type="showConfirmPassword ? 'text' : 'password'"
             :append-inner-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append-inner="showConfirmPassword = !showConfirmPassword"
@@ -45,6 +45,7 @@
 import { useUser } from "@/composables/user";
 import { ref, computed } from "vue";
 import Page from "../../components/shared/Page/page.vue";
+import { editProfile } from "@/api/users";
 const { user } = useUser();
 const username = ref(user.name);
 const email = ref(user.email);
@@ -54,9 +55,43 @@ const showConfirmPassword = ref(false);
 const password = ref("");
 const confirmPassword = ref("");
 const fieldRules = [(v: string) => !!v || "Campo obrigatório"];
-const confirmPasswordRules = computed(() => [
-  (value: any) => !!value || "Confirm Password is required",
-  (value: string) => value === password.value || "Passwords do not match",
-]);
-const onSubmit = (values: any) => console.log(values);
+
+const passwordRules = [
+  (value: string) => password.value === value || "Passwords do not match."
+];
+
+const requiredIfNotEmpty = () => {
+  if (password.value !== "" || confirmPassword.value !== "") {
+    return [(v: any) => !!v || "This field is required."];
+  }
+  return [];
+};
+const rules = computed(() => [...requiredIfNotEmpty(), ...passwordRules]);
+
+const onSubmit = async () => {
+  const { valid } = await form.value.validate();
+
+  if (valid) {
+    const data = {
+      username,
+      email,
+      password: "",
+      password_confirm: "",
+      // password,
+      // password_confirm: confirmPassword,
+      token: user.token,
+    };
+    editProfile(data).then((resp) => {
+      if (resp.success) {
+        //updateUserName(resp.data);
+        console.log(resp.data);
+      } else {
+        // because this is mock we need mock a successful login
+        // updateUserName(email.value); // delete this (mock)
+        // closeModal(); // delete this (mock)
+        console.log("erro");
+      }
+    });
+  }
+};
 </script>
