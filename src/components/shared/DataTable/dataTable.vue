6db4b6afdab0028ba0d64c09ff1664f0fb85c2a6
joaorga1 <joaorga1@gmail.com>
2023-06-26 08:43:52 +0100

    fix items per page text

--- src/components/shared/DataTable/dataTable.vue
+++ src/components/shared/DataTable/dataTable.vue
@@ -2,6 +2,7 @@
   <v-data-table
     :items-per-page="itemsPerPage"
     :items-per-page-options="itemsPerPageOptions"
+    items-per-page-text="cenas"
     :headers="headers"
     :items="serverItems"
     item-value="name"
@@ -98,7 +99,7 @@ import {
 import { VDataTable } from 'vuetify/labs/VDataTable';
 import ErrorSuccessMessage from '../ErrorSuccessMessages/errorSuccessMessages.vue';
 export default defineComponent({
-  name: 'UsersDataTable',
+  name: 'GenericDataTable',
   components: {
     ErrorSuccessMessage,
   },
@@ -135,7 +136,8 @@ const itemsPerPageOptions = ref([
   { value: 15, title: '15' },
   { value: 20, title: '20' },
   { value: 50, title: '50' },
-  { value: -1, title: '$vuetify.dataFooter.itemsPerPageAll' },
+  { value: -1, title: 'Todos' },
+  //{ value: -1, title: '$vuetify.dataFooter.itemsPerPageAll' },
 ]);
 const itemsPerPage = ref(10);
 const serverItems = ref([]);
