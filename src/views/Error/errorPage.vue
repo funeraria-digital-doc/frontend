60f392d5a4cbb531acd1058b9d6dd0f4eb475f79
joaorga1 <123202392+joaorga1@users.noreply.github.com>
2023-09-24 12:57:55 +0100

    error page for 404 and 500 errors (#24)

---
+++ src/views/Error/errorPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <page title="">
+    <div v-html="message" class="text-center"></div>
+    <div class="text-center">
+      <img src="/error-icon.png" class="img-max" />
+    </div>
+  </page>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { onBeforeMount } from 'vue';
+export default defineComponent({
+  name: 'ErrorPage',
+});
+</script>
+<script lang="ts" setup>
+import Page from '../../components/shared/Page/page.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const message = ref('');
+onBeforeMount(() => {
+  console.log(route.meta.status);
+  if (route.meta.status === 'service-unavailable') {
+    message.value =
+      '<h1>Serviço temporáriamente indisponível.</br>' +
+      'Tente novamente mais tarde.</h1> </br> ' +
+      '<h3>Se o problema presistir, contacte os administradores do sistema.</h3>';
+  }
+
+  if (route.meta.status === 'not-found') {
+    message.value =
+      '<h1>Endereço não encontrado.</h1> </br> ' +
+      '<h3>Se o problema presistir, contacte os administradores do sistema.</h3>';
+  }
+});
+</script>
+<style scoped>
+.img-max {
+  max-width: 350px;
+  width: 35%;
+  margin-top: 6rem;
+  height: auto;
+}
+</style>
