52a34a19e73456dba54a1b250e13a258e3c400a9
jgalves <joaorga1@gmail.com>
2023-09-26 09:36:02 +0100

    added home page stats

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -1,12 +1,20 @@
 <template>
   <page title="Página inicial">
+    <div class="spacer-2"></div>
     <p>
       Plataforma para agilizar e gerir os documentos referentes ao processo de
       declarar um falecimento.
     </p>
+    <home-stats></home-stats>
   </page>
 </template>
 
 <script lang="ts" setup>
 import Page from '../../components/shared/Page/page.vue';
+import HomeStats from './components/homeStats.vue'
 </script>
+<style scoped>
+.spacer-2{
+  height: 2rem
+}
+</style>
