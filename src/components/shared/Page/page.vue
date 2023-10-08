a7eeb8bc3cfccf6c85d7cb9abbbc52a1f3d51c2d
joaorga1 <123202392+joaorga1@users.noreply.github.com>
2023-10-08 16:12:15 +0100

    added back button (#32)

--- src/components/shared/Page/page.vue
+++ src/components/shared/Page/page.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="page">
-    <h1 class="page__title text-h6 text-md-h5 text-lg-h4">{{ title }}</h1>
+    <div class="d-flex" style="justify-content: space-between">
+      <h1 class="page__title text-h6 text-md-h5 text-lg-h4">{{ title }}</h1>
+      <v-btn
+        v-if="back"
+        type="button"
+        dark
+        class="mb-2 d-flex align-self-center"
+        color="primary"
+        @click="backRedirect(back)"
+        >Voltar</v-btn
+      >
+    </div>
+
     <!-- renders wrapped content -->
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts" setup>
+import router from '@/router';
+
 defineProps({
   title: {
     type: String,
     required: true,
   },
+  back: {
+    type: String,
+    required: false,
+  },
 });
 </script>
-<script lang="ts"></script>
+<script lang="ts">
+function backRedirect(link: string) {
+  router.push({ name: link });
+}
+</script>
 
 <style lang="scss">
 .page {
