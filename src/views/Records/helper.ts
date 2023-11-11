778b436d352c09ba3d6cfb3a51523a36cade3efe
jgalves <joaorga1@gmail.com>
2023-11-11 08:56:06 +0000

    waiting decision on time default value

--- src/views/Records/helper.ts
+++ src/views/Records/helper.ts
@@ -18,7 +18,7 @@ export const getRecords = async (
             family_member_phone: record.family_member_phone,
             gender: getLabel('gender', record.gender, fields),
             status: getLabel('status', record.status, fields),
-            group_id: getLabel('group_id', record.group_id, fields)
+            group_id: getLabel('group_id', record.group_id, fields),
           };
         });
       }
@@ -115,44 +115,35 @@ export function generateDocuments(record: any, callback: Function) {
   callback(record);
 }
 
-export function getFormat(
-  fieldName: String,
-  templateValidations: any,
-  toPicker: boolean
-) {
-  const selectedVal = templateValidations.find(
-    (templateVal: { name: String }) => {
-      return templateVal.name === fieldName;
-    }
-  );
+export function getFormat(templateFormat: any, toPicker: boolean) {
   let format = '';
-  if (['DAY', 'DAYS'].some((sub) => selectedVal.format.includes(sub))) {
+  if (['DAY', 'DAYS'].some((sub) => templateFormat.includes(sub))) {
     if (format) {
       format += '/';
     }
     format += toPicker ? 'dd' : 'DD';
   }
-  if (['MONTH', 'MONTHS'].some((sub) => selectedVal.format.includes(sub))) {
+  if (['MONTH', 'MONTHS'].some((sub) => templateFormat.includes(sub))) {
     if (format) {
       format += '/';
     }
     format += 'MM';
   }
-  if (['YEAR', 'YEARS'].some((sub) => selectedVal.format.includes(sub))) {
+  if (['YEAR', 'YEARS'].some((sub) => templateFormat.includes(sub))) {
     if (format) {
       format += '/';
     }
     format += 'YYYY';
   }
-  if (['HOUR', 'HOURS'].some((sub) => selectedVal.format.includes(sub))) {
+  if (['HOUR', 'HOURS'].some((sub) => templateFormat.includes(sub))) {
     if (format) {
       format += ' ';
     }
     format += 'HH';
   }
-  if (['MINUTE', 'MINUTES'].some((sub) => selectedVal.format.includes(sub))) {
+  if (['MINUTE', 'MINUTES'].some((sub) => templateFormat.includes(sub))) {
     if (format) {
-      if (['HOUR', 'HOURS'].some((sub) => selectedVal.format.includes(sub))) {
+      if (['HOUR', 'HOURS'].some((sub) => templateFormat.includes(sub))) {
         format += ':';
       } else {
         format += ' ';
@@ -160,11 +151,11 @@ export function getFormat(
     }
     format += 'mm';
   }
-  if (['SECOND', 'SECONDS'].some((sub) => selectedVal.format.includes(sub))) {
+  if (['SECOND', 'SECONDS'].some((sub) => templateFormat.includes(sub))) {
     if (format) {
       if (
         ['MINUTE', 'MINUTES', 'HOUR', 'HOURS'].some((sub) =>
-          selectedVal.format.includes(sub)
+          templateFormat.includes(sub)
         )
       ) {
         format += ':';
