14b8be73c33046cb21bbcaffad32955152e7563d
jgalves <joaorga1@gmail.com>
2023-12-06 07:54:18 +0000

    added declaration_number field on records form

--- src/views/Stats/helper.ts
+++ src/views/Stats/helper.ts
@@ -4,34 +4,46 @@ export function updateLineChartData(
   series: any,
   addAmount: number
 ) {
-  const seriesData: any = resp.data
-    .filter((item: { categories: any; data: any }) => {
-      return (
-        item &&
-        item.data !== null &&
-        item.data !== undefined &&
-        isFinite(item.data)
-      );
-    })
-    .map((item: { categories: any; data: any }) => {
-      return { x: item.categories, y: item.data };
-    });
-  const yAxis = seriesData.map((item) => item.y);
-  if (seriesData.length == 1) {
-    seriesData.push({ x: new Date(), y: yAxis });
-  }
+  if (resp.data.length > 0) {
+    const seriesData: any = resp.data
+      .filter((item: { categories: any; data: any }) => {
+        return (
+          item &&
+          item.data !== null &&
+          item.data !== undefined &&
+          isFinite(item.data)
+        );
+      })
+      .map((item: { categories: any; data: any }) => {
+        return { x: item.categories, y: item.data };
+      });
+    const yAxis = seriesData.map((item) => item.y);
+    if (seriesData.length == 1) {
+      seriesData.push({ x: new Date(), y: yAxis });
+    }
 
-  const maxYAxis = Math.ceil(Math.max.apply(null, yAxis)) + addAmount;
-  const tickAmount = 10;
-  options.value = {
-    ...options.value,
-    yaxis: {
-      max: maxYAxis,
-      min: 0,
-      tickAmount: tickAmount,
-    },
-  };
-  series.value = [{ ...series.value[0], data: seriesData }];
+    const maxYAxis = Math.ceil(Math.max.apply(null, yAxis)) + addAmount;
+    const tickAmount = 10;
+    options.value = {
+      ...options.value,
+      yaxis: {
+        max: maxYAxis,
+        min: 0,
+        tickAmount: tickAmount,
+      },
+    };
+    series.value = [{ ...series.value[0], data: seriesData }];
+  } else{
+    options.value = {
+      ...options.value,
+      yaxis: {
+        max: 0,
+        min: 0,
+        tickAmount: 0,
+      },
+    };
+    series.value = [{ ...series.value[0], data: [] }];
+  }
 }
 
 export function updatePieChartData(resp: any, options: any, series: any) {
